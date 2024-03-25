7d92b6332533f5ff3f41163e6eef158ce8918486
HIS <hwanginsick@gmail.com>
2024-03-25 11:34:28 +0900

    refactor: 폴더 구조 변경에 따른 브랜치 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
 import Ticketing from "./pages/ticketing/Ticketing";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/"></Route>
           <Route path="/ticketing" element={<Ticketing />} />
