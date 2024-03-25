7758944a9748af5e662e45fea848b80c1aad8c8b
yeri <leeye1025@gmail.com>
2024-03-25 17:45:18 +0900

    feat: App.js_Header추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Ticketing from "./pages/ticketing/Ticketing";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Ticketing from './pages/ticketing/Ticketing';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/"></Route>
           <Route path="/ticketing" element={<Ticketing />} />
