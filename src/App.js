765088ad96508bf3feeb131b2f5ece36b1bba0a2
YeoniSon <sjy8211@gmail.com>
2024-03-26 22:46:11 +0900

    feat: manage login 생성
    manageLogin page 작성후,
    localhost:8080/manage 로 연결
    header가 안 없어지네요.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Ticketing from "./pages/ticketing/Ticketing";
-import Homepage from "./pages/homepage/HomePage";
-import MyPage from "./pages/mypage/MyPage";
-import WriteReview from "./components/WriteReview";
-import ViewAll from "./pages/homepage/ViewAll";
-import Community from "./pages/community/Community";
+// import Ticketing from "./pages/ticketing/Ticketing";
+// import Homepage from "./pages/homepage/HomePage";
+// import MyPage from "./pages/mypage/MyPage";
+// import WriteReview from "./components/WriteReview";
+// import ViewAll from "./pages/homepage/ViewAll";
+// import Community from "./pages/community/Community";
 import LoginPage from "./pages/LoginPage";
+import ManageLoginPage from "./pages/ManageLoginPage";
 
 function App() {
   const pathname = window.location.pathname;
@@ -21,13 +22,14 @@ function App() {
       <BrowserRouter>
         {showHF && <Header />}
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/" element={<Homepage />} />
           <Route path="/writereview" element={<WriteReview />} />
           <Route path="/community" element={<Community />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/viewall" element={<ViewAll />} />
-          <Route path="/ticketing" element={<Ticketing />} />
+          <Route path="/ticketing" element={<Ticketing />} /> */}
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/manage" element={<ManageLoginPage />} />
         </Routes>
         {showHF && <Footer />}
       </BrowserRouter>
