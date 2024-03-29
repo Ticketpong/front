6df94c3fd54f671ad6cf8d9608917e12310cdbc8
jeasik <kives2004@gmail.com>
2024-03-29 13:43:59 +0900

    style: 커뮤니티 사이드 수정 및 홈페이지 컴포넌트 간격 조절, 폰트적용, 백그라운드 수정

--- src/features/HomePage/HPContent2.js
+++ src/features/HomePage/HPContent2.js
@@ -8,6 +8,8 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 120px auto;
+  min-width: 1358px;
+  height: 910px;
 `;
 
 const Strong = styled.strong`
@@ -18,7 +20,7 @@ const Strong = styled.strong`
 
 const CategoryBtn = styled.div`
   display: flex;
-  margin-bottom: 50px;
+  margin-bottom: 80px;
 `;
 
 const CategoryButton = styled.button`
@@ -33,6 +35,7 @@ const CategoryButton = styled.button`
   margin-left: 15px;
   cursor: pointer;
   border-radius: 24px;
+  line-height: 10px;
 
   &:focus,
   :hover {
@@ -54,15 +57,23 @@ const SliderContainer = styled.div`
   white-space: nowrap;
 `;
 
+const UlContainer = styled.div`
+  min-width: 1300px;
+`;
+
 const SlideButton = styled.button`
   position: absolute;
-  top: 50%;
+  top: 45%;
+  width: 70px;
+  height: 70px;
   transform: translateY(-50%);
   background-color: white;
   border: none;
-  font-size: 50px;
+  font-size: 40px;
+  color: #fc1055;
   cursor: pointer;
   border-radius: 50%;
+  z-index: 999;
   ${(props) => (props.direction === "left" ? "left: 0;" : "right: 0;")}
 
   &:focus {
@@ -72,7 +83,8 @@ const SlideButton = styled.button`
 
 const ListItem = styled.li`
   display: inline-block;
-  margin-right: 60px;
+  margin-right: 40px;
+  margin-left: 40px;
   vertical-align: top;
 `;
 
@@ -83,8 +95,8 @@ const ImageContainer = styled.div`
 
 const Image = styled.img`
   display: block;
-  width: 320px;
-  height: 420px;
+  min-width: 350px;
+  height: 450px;
   margin-bottom: 10px;
   object-fit: cover;
   border-radius: 12px;
@@ -92,15 +104,17 @@ const Image = styled.img`
 
 const Rank = styled.span`
   position: absolute;
-  bottom: 5px;
+  bottom: -10px;
   left: 5px;
   background-color: none;
   color: white;
   padding: 5px;
   border-radius: 5px;
-  border-radius: 5px;
   font-size: 48px;
-  font-weight: bold;
+  font-family: "Roboto", sans-serif;
+  font-weight: 700;
+  font-style: italic;
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 `;
 
 const Text = styled.p`
@@ -110,6 +124,24 @@ const Text = styled.p`
   text-overflow: ellipsis;
   white-space: nowrap;
   max-width: ${(props) => (props.over ? "350px" : "none")};
+`;
+
+const StyledViewAllButton = styled.button`
+  margin-top: 70px;
+  width: 240px;
+  height: 60px;
+  border-radius: 50px;
+  background-color: white;
+  color: #fc1055;
+  border: 1px solid #fc1055;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  &:focus {
+    background-color: #fc1055;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+    color: #ffffff;
+  }
 `;
 
 const HPContent2 = ({ item, onClick }) => {
@@ -178,7 +210,7 @@ const HPContent2 = ({ item, onClick }) => {
         >
           {"<"}
         </SlideButton>
-        <ul>
+        <UlContainer>
           {displayedData.map((item, index) => (
             <ListItem key={index} onClick={() => onClick(item)}>
               <Link to={`/ticketing/${item.id}`}>
@@ -195,7 +227,7 @@ const HPContent2 = ({ item, onClick }) => {
               <Text>기간: {item.prfpd._text}</Text>
             </ListItem>
           ))}
-        </ul>
+        </UlContainer>
         <SlideButton
           className="nextButton"
           direction="right"
@@ -204,6 +236,9 @@ const HPContent2 = ({ item, onClick }) => {
           {">"}
         </SlideButton>
       </SliderContainer>
+      <Link to="/viewall">
+        <StyledViewAllButton>티켓 전체보기</StyledViewAllButton>
+      </Link>
     </Container>
   );
 };
