523238246e7d114c6e9a686dbae2c838a243ddb7
HIS <hwanginsick@gmail.com>
2024-04-04 16:27:52 +0900

    feat:할인 카드별 결제 시 할인율 적용

--- src/features/Ticketing/TicketingPayment.js
+++ src/features/Ticketing/TicketingPayment.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export const PongButton = styled.button`
@@ -30,7 +31,7 @@ export const PongButton = styled.button`
     border-radius: 8px;
   }
 `;
-const Payment = ({ amount, prfnm }) => {
+const Payment = ({ amount, showData, selectedSeat, people, cardData }) => {
   useEffect(() => {
     const iamport = document.createElement("script");
     iamport.src = "https://cdn.iamport.kr/v1/iamport.js";
@@ -40,6 +41,19 @@ const Payment = ({ amount, prfnm }) => {
     };
   }, []);
 
+  const getDate = () => {
+    const today = new Date();
+    const year = today.getFullYear(); // 2023
+    const month = (today.getMonth() + 1).toString().padStart(2, "0"); // 06
+    const day = today.getDate().toString().padStart(2, "0"); // 18
+
+    const dateString = year + "-" + month + "-" + day; // 2023-06-18
+
+    return dateString;
+  };
+
+  const navigate = useNavigate();
+
   const onClickPayment = () => {
     const { IMP } = window;
     IMP.init("imp83485354");
@@ -47,14 +61,37 @@ const Payment = ({ amount, prfnm }) => {
     const data = {
       pg: "kcp.AO09C", // PG사 코드.PG상점아이디 //고정값
       pay_method: "card", // 결제수단 //고정값
-      merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
+      merchant_uid: `mid_${new Date().getTime()}`, // 주문번호 uid도 추가해서 고유값 만들기
       amount: amount, // 결제금액
-      name: `${prfnm}`, // 주문명
+      name: `${showData.showData.prfnm}`, // 주문명
       buyer_name: "홍길동", // 구매자 이름
       buyer_tel: "01012341234", // 구매자 전화번호
       buyer_email: "example@example.com", // 구매자 이메일
       buyer_addr: "신사동 661-16", // 구매자 주소
+
+      custom_data: {
+        user_id: "user0002",
+        mt20id: showData.showData.mt20id,
+        mt10id: showData.showData.mt10id,
+        selectedSeat: selectedSeat,
+        res_date: getDate(),
+        people: people,
+        selectdate: showData.timeData.playDate,
+        selecttime: showData.timeData.playTime,
+        watchstate: false,
+        prestate: false,
+      },
     };
+
+    if (cardData) {
+      // cardData가 존재하는 경우 추가 카드 정보를 포함하여 결제 요청
+      data.card = {
+        direct: {
+          code: cardData.code,
+        },
+      };
+    }
+
     IMP.request_pay(data, callback);
   };
 
@@ -62,6 +99,7 @@ const Payment = ({ amount, prfnm }) => {
     if (response.success) {
       alert("결제 성공!");
       console.log(response);
+      navigate(`/ticketing/${showData.showData.mt20id}`);
     } else {
       alert(`결제 실패! : ${response.error_msg}`);
     }
