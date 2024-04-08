05a325f4ab980f3751e2a9a9bea5c3feb16b24b1
Jaesik Yang <kives2004@gmail.com>
2024-04-08 15:35:00 +0900

    build: Performance 로딩속도 개선을 위한 스타일드 컴포넌트 분리

--- src/pages/performance/Performance.js
+++ src/pages/performance/Performance.js
@@ -1,18 +1,9 @@
 import React, { useState, useEffect } from "react";
+import * as S from "../../styles/PerformanceStyled";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import Img from "../../assets/ViewAllImg/Image20240328164620.jpg";
 import axios from "axios";
-// import Geolocation from "../../components/base/GeoLocation";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 80px;
-  text-align: center;
-  max-width: 100%;
-`;
+import Img from "../../assets/ViewAllImg/Image20240328164620.jpg";
+import { Link } from "react-router-dom";
 
 const BackgroudImg = styled.image`
   background-image: url(${Img});
@@ -23,257 +14,6 @@ const BackgroudImg = styled.image`
   align-items: center;
   min-width: 100%;
   overflow: hidden;
-`;
-
-// ======================================================================================================================
-// 상단 부분
-const Upper = styled.div`
-  margin-top: 220px;
-`;
-
-const ShortHr = styled.hr`
-  width: 50px;
-  height: 1px;
-  background-color: #ffffff;
-  margin-top: 15px;
-  border: 0;
-`;
-const All = styled.span`
-  font-size: 40px;
-  font-weight: bold;
-  color: black;
-  text-align: center;
-  align-items: center;
-  margin: 10px auto;
-  color: #ffffff;
-`;
-
-const UpperUL = styled.ul`
-  display: inline-block;
-  flex-wrap: wrap;
-  justify-content: center;
-  list-style-type: none;
-  padding: 0;
-  margin: 20px 0;
-  flex-wrap: wrap;
-  min-width: 1600px;
-  max-width: 1600px;
-`;
-
-const UpperLI = styled.li`
-  flex: 0 0 calc(20% - 20px);
-  margin: 10px;
-  box-sizing: border-box;
-  display: inline-block;
-  margin-right: 20px;
-  vertical-align: top;
-  min-width: 290px;
-  max-width: 290px;
-`;
-
-const UpperImage = styled.div`
-  display: block;
-  min-width: 270px;
-  height: 360px;
-  margin-bottom: 10px;
-  object-fit: cover;
-  img {
-    max-height: 100%;
-    min-width: 285px;
-    border-radius: 12px;
-  }
-`;
-
-const UpperP = styled.p`
-  margin: 0;
-  font-size: ${(props) => (props.over ? "18px" : "inherit")};
-  overflow: hidden;
-  text-align: left;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  max-width: ${(props) => (props.over ? "310px" : "inherit")};
-`;
-
-const UpperOver = styled.p`
-  margin: 0;
-  font-size: 20px;
-  font-weight: bold;
-  overflow: hidden;
-  text-align: left;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  max-width: ${(props) => (props.over ? "310px" : "inherit")};
-  margin-bottom: 4px;
-`;
-const CategoryContainer = styled.div`
-  display: flex;
-  margin-bottom: 55px;
-  align-items: center;
-  justify-content: center;
-  margin-right: 22px;
-`;
-// 이미지 순위 처리
-const ImageContainer = styled.div`
-  position: relative;
-  min-width: 290px;
-  max-width: 290px;
-  max-width: 300px;
-  max-height: 410px;
-`;
-
-const Rank = styled.p`
-  position: absolute;
-  bottom: -50px;
-  left: 10px;
-  background-color: none;
-  color: white;
-  text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black,
-    1px 1px 0 black;
-  padding: 5px;
-  border-radius: 5px;
-  border-radius: 5px;
-  font-size: 54px;
-  font-family: "Roboto", sans-serif;
-  font-weight: 700;
-  font-style: italic;
-  @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
-`;
-
-const Button = styled.button`
-  min-width: 120px;
-  height: 50px;
-  background-color: #373a42;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  margin-top: 100px;
-  margin-bottom: 10px;
-  margin-right: 15px;
-  margin-left: 15px;
-  cursor: pointer;
-  border-radius: 24px;
-  line-height: 8px;
-  font-size: 24px;
-  font-weight: bold;
-
-  &:focus {
-    background-color: #fc1055;
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-  }
-`;
-
-const GeographyButton = styled.button`
-  min-width: 120px;
-  height: 50px;
-  background-color: #373a42;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  margin-top: 100px;
-  margin-bottom: 10px;
-  margin-right: 15px;
-  margin-left: 15px;
-  cursor: pointer;
-  border-radius: 24px;
-  line-height: 8px;
-  font-size: 24px;
-  font-weight: bold;
-
-  &:focus {
-    background-color: #fc1055;
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-  }
-`;
-
-const MoreButton = styled.button`
-  min-width: 240px;
-  height: 60px;
-  background-color: #ffffff;
-  color: #fc1055;
-  border: 1px solid #fc1055;
-  padding: 10px 20px;
-  margin-top: 50px;
-  margin-bottom: 10px;
-  margin-right: 15px;
-  margin-left: 15px;
-  cursor: pointer;
-  border-radius: 24px;
-  font-size: 24px;
-
-  &:focus {
-    background-color: #fc1055;
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-    color: #ffffff;
-  }
-`;
-
-// =============================================================================================================
-// 하단 부분
-
-const Bottom = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-width: 1600px;
-  max-width: 1600px;
-  margin: 80px auto;
-`;
-
-const StyledUL = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-`;
-
-const StyledLI = styled.li`
-  align-items: center;
-  justify-content: center;
-  margin: 10px;
-  display: inline-block;
-  vertical-align: top;
-  margin-bottom: 80px;
-  margin-left: 24px;
-  margin-right: 24px;
-  min-width: 350px;
-  min-height: 350px;
-`;
-
-const StyledImage = styled.div`
-  display: block;
-  min-width: 330px;
-  max-width: 330px;
-  height: 390px;
-  margin-bottom: 10px;
-  object-fit: cover;
-  img {
-    min-height: 100%;
-    max-height: 100%;
-    min-width: 100%;
-    max-width: 100%;
-    border-radius: 12px;
-  }
-`;
-
-const StyledP = styled.p`
-  margin: 0;
-  font-size: 18px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  text-align: left;
-  max-width: ${(props) => (props.over ? "350px" : "inherit")};
-`;
-
-const StyledOver = styled.p`
-  margin: 0;
-  font-size: 20px;
-  font-weight: bold;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  text-align: left;
-  max-width: ${(props) => (props.over ? "350px" : "inherit")};
 `;
 
 const StyleLink = styled(Link)`
@@ -428,79 +168,87 @@ const Performance = () => {
 
   return (
     <>
-      <Container>
+      <S.Container>
         <BackgroudImg>
-          <Upper>
-            <All>공연 / 전시</All>
-            <ShortHr />
-            <UpperUL>
+          <S.Upper>
+            <S.All>공연 / 전시</S.All>
+            <S.ShortHr />
+            <S.UpperUL>
               {rankDisplayedData.map((item, index) => {
                 if (!showAll && index >= 5) return null;
                 return (
-                  <UpperLI key={index}>
-                    <Link to={`/ticketing/${item.mt20id}`}>
-                      <ImageContainer>
-                        <UpperImage>
+                  <S.UpperLI key={index}>
+                    <StyleLink to={`/ticketing/${item.mt20id}`}>
+                      <S.ImageContainer>
+                        <S.UpperImage>
                           <img src={item.poster} alt="포스터" />
-                          <Rank>{startIndex + index + 1}</Rank>
-                        </UpperImage>
-                      </ImageContainer>
-                    </Link>
-                    <UpperOver>
+                          <S.Rank>{startIndex + index + 1}</S.Rank>
+                        </S.UpperImage>
+                      </S.ImageContainer>
+                    </StyleLink>
+                    <S.UpperOver>
                       {item.prfnm.slice(0, 17)}
                       {item.prfnm.length > 20 ? "..." : ""}
-                    </UpperOver>
-                    <UpperP>
+                    </S.UpperOver>
+                    <S.UpperP>
                       {item.prfpdfrom}~{item.prfpdto}
-                    </UpperP>
-                  </UpperLI>
+                    </S.UpperP>
+                  </S.UpperLI>
                 );
               })}
-            </UpperUL>
-          </Upper>
+            </S.UpperUL>
+          </S.Upper>
         </BackgroudImg>
-      </Container>
+      </S.Container>
 
-      <CategoryContainer>
-        <Button onClick={() => handleCategoryChange("전체")}>전체보기</Button>
-        <Button onClick={() => handleCategoryChange("연극")}>연극</Button>
-        <Button onClick={() => handleCategoryChange("뮤지컬")}>뮤지컬</Button>
-        <Button onClick={() => handleCategoryChange("대중음악")}>콘서트</Button>
-        <Button onClick={() => handleCategoryChange("기타")}>기타</Button>
-        <GeographyButton onClick={handleGeographyChange}>
+      <S.CategoryContainer>
+        <S.Button onClick={() => handleCategoryChange("전체")}>
+          전체보기
+        </S.Button>
+        <S.Button onClick={() => handleCategoryChange("연극")}>연극</S.Button>
+        <S.Button onClick={() => handleCategoryChange("뮤지컬")}>
+          뮤지컬
+        </S.Button>
+        <S.Button onClick={() => handleCategoryChange("대중음악")}>
+          콘서트
+        </S.Button>
+        <S.Button onClick={() => handleCategoryChange("기타")}>기타</S.Button>
+        <S.GeographyButton onClick={handleGeographyChange}>
           위치기반
-        </GeographyButton>
-      </CategoryContainer>
+        </S.GeographyButton>
+      </S.CategoryContainer>
 
-      <Bottom>
-        <StyledUL>
+      <S.Bottom>
+        <S.StyledUL>
           {displayedData2.map((item, index) => {
             if (!showAll && index >= 8) return null;
             return (
               <StyleLink to={`/ticketing/${item.mt20id}`}>
-                <StyledLI key={index}>
-                  <StyledImage>
+                <S.StyledLI key={index}>
+                  <S.StyledImage>
                     <img src={item.poster} alt="포스터" />
-                  </StyledImage>
-                  <StyledOver>
+                  </S.StyledImage>
+                  <S.StyledOver>
                     {item.genrenm} &nbsp;
                     {item.prfnm.length > 9 ? (
                       <>&lt;{item.prfnm.slice(0, 9)}...</>
                     ) : (
                       <>&lt;{item.prfnm}&gt;</>
                     )}
-                  </StyledOver>
-                  <StyledP>{item.fcltynm}</StyledP>
-                  <StyledP>
+                  </S.StyledOver>
+                  <S.StyledP>{item.fcltynm}</S.StyledP>
+                  <S.StyledP>
                     {item.prfpdfrom}~{item.prfpdto}
-                  </StyledP>
-                </StyledLI>
+                  </S.StyledP>
+                </S.StyledLI>
               </StyleLink>
             );
           })}
-        </StyledUL>
-        {!showAll && <MoreButton onClick={handleLoadMore}>더보기</MoreButton>}
-      </Bottom>
+        </S.StyledUL>
+        {!showAll && (
+          <S.MoreButton onClick={handleLoadMore}>더보기</S.MoreButton>
+        )}
+      </S.Bottom>
     </>
   );
 };
