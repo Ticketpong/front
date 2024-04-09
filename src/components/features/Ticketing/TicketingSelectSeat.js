229e74d968ed275707a7af3b91eb4d5cbaf3eb5d
HIS <hwanginsick@gmail.com>
2024-04-09 17:58:47 +0900

    feat: 로그인 여부에 따라 예매 가능 여부 설정

--- src/components/features/Ticketing/TicketingSelectSeat.js
+++ src/components/features/Ticketing/TicketingSelectSeat.js
@@ -1,10 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PongButton } from "./TicketingPayment";
 import SeatSelectionModal from "./SeatSelectionModal";
+import axiosWithAuth from "../../base/axiosWithAuth";
+import { useNavigate } from "react-router-dom";
 
 const TicketingSelectSeat = (showData) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [isLogined, setIsLogined] = useState(false);
+
+  useEffect(() => {
+    const fetchLoginStatus = async () => {
+      try {
+        const response = await axiosWithAuth().get(
+          "http://localhost:8080/login/profile"
+        );
+        const { id, isLogined } = response.data;
+        if (isLogined) {
+          setUserId(id);
+          setIsLogined(true);
+        }
+      } catch (error) {
+        console.error("로그인 상태를 확인하는 동안 오류 발생:", error);
+      }
+    };
+
+    fetchLoginStatus();
+  }, []);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -18,15 +41,25 @@ const TicketingSelectSeat = (showData) => {
     setSelectedSeats(seats);
   };
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <PongButton onClick={handleOpenModal}>좌석 선택</PongButton>
-      <SeatSelectionModal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        onSelect={handleSelectSeats}
-        showData={showData}
-      />
+      {isLogined ? (
+        <>
+          <PongButton onClick={handleOpenModal}>좌석 선택</PongButton>
+          <SeatSelectionModal
+            isOpen={isModalOpen}
+            onClose={handleCloseModal}
+            onSelect={handleSelectSeats}
+            showData={showData}
+          />
+        </>
+      ) : (
+        <PongButton onClick={() => navigate("/login")} className="payment">
+          로그인
+        </PongButton>
+      )}
     </>
   );
 };
