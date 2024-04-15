ea103886d5a6118e6bc3f412ca297bb0f21b8ca2
Uddoro <lowell0322@naver.com>
2024-04-15 14:08:47 +0900

    chore: managepage 수정 완료

--- src/components/features/ManagePage/ReviewManage.js
+++ src/components/features/ManagePage/ReviewManage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ReviewsTable from "./ReviewDetail";
 import axios from "axios";
